bd16debe7d42b65448607257cae2db0949321389
Masum Billah <masumbillah2062003@gmail.com>
2024-04-09 23:54:01 +0600

    json data added and some code problem solve has been added to the git hub repository

--- src/Component/Bed.jsx
+++ src/Component/Bed.jsx
@@ -1,32 +1,58 @@
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
-const Bed = () => {
+
+const Bed = ({ data }) => {
+    const {catchUp} = useContext(AuthContext)
+  const {
+    image,
+    hotel_title,
+    id,
+    Speciality_name,
+    price,
+    location,
+    facilities,
+    button,
+  } = data;
+  console.log(data);
+
   return (
-    // to={`/book/${id}`}
-    <div className="lg:mt-16 mt-8 grid lg:grid-cols-3 grid-cols-1 gap-6">
-      <Link>
-        <div className="w-80 lg:w-full p-6 rounded-2xl border-[#13131326] space-y-6">
-          <figure className="bg-[#F3F3F3] h-56 rounded-2xl flex justify-center items-center">
-            <img src="" alt="comming" className=" w-[full] h-40" />
-          </figure>
-          <div className="space-y-4">
-            <div className="flex items-center gap-10 text-[#23BE0A] font-medium">
-              <p className="bg-[#23BE0A0D] rounded-[30px] py-2 px-4">#some</p>
-              <p className="bg-[#23BE0A0D] rounded-[30px] py-2 px-4">#some</p>
-            </div>
-            <h2 className="text-2xl font-bold">title</h2>
-            <p className="font-medium text-[#131313CC]">By : bed</p>
-            <div className="flex items-center justify-between text-[#131313CC] font-medium pt-6 border-t border-dashed">
-              <p>al</p>
-              <p className="flex items-center gap-2">
-                {/* <CiStar /> {rating} */}
-              </p>
-            </div>
+    <div>
+      <div className="w-80 lg:w-full p-6 rounded-2xl border-2 border-[#023222]  space-y-6">
+        <figure className="bg-[#F3F3F3] w-[440px] h-[220px] rounded-2xl flex justify-center items-center">
+          <img
+            src={image}
+            alt="comming"
+            className="w-full h-full rounded-2xl"
+          />
+        </figure>
+        <div className="space-y-4 ">
+          <div className="flex items-center gap-10 text-[#023222] font-medium">
+            {facilities.map((data, idx) => (
+              <div key={idx}>
+                <p className="bg-[#0232226d] text-[#023222] rounded-[30px] py-2 px-2"> 
+                  #{data}
+                </p>
+              </div>
+            ))}
+          </div>
+          <h2 className="text-2xl font-bold">{hotel_title}</h2>
+          <h2 className="text-xl font-bold">{Speciality_name}</h2>
+          <p className="font-medium text-[#023222]">Price : {price}</p>
+          <div className="flex items-center justify-between text-[#023222] font-medium pt-6 border-t border-dashed">
+            <p>Location : {location}</p>
+            <Link to={catchUp ? `/data/${id}` : '/login'}><button className="btn bg-[#023222] text-white hover:bg-white border-2 hover:border-[#023222] hover:text-[#023222]">{button}</button></Link>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
 
 export default Bed;
+
+Bed.propTypes = {
+    data: PropTypes.object
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,19 +5,26 @@ import "swiper/css";
 import "../Pages/home.css";
 import Food from "../Component/Food";
 
+import { useLoaderData } from "react-router-dom";
+
 const Home = () => {
+  const allData = useLoaderData();
+  console.log(allData)
   return (
     <div>
       <div className="bg"></div>
-      <div className="bg-slate-100">
+      <div className="bg-slate-100 pb-12">
         <Slider></Slider>
-        <div>
-          <Bed></Bed>
+        <div className="lg:mt-16 mt-8 grid lg:grid-cols-3 grid-cols-1 gap-6">
+          {
+            allData.map((data) => <Bed key={data.id} data={data}></Bed> )
+          }
+          
         </div>
       </div>
       <div className="w-full h-screen flex justify-center items-center">
-          <Food></Food>
-        </div>
+        <Food></Food>
+      </div>
     </div>
   );
 };
